48eba8a24bade10d2ac0d859e112df6721a7fde7
ysb4642 <ysb4642@naver.com>
2022-02-13 01:04:16 +0900

    22_02_13

---
+++ ch2/src/ch2/SpecialCharEx.java
@@ -0,0 +1,12 @@
+package ch2;
+
+public class SpecialCharEx {
+	public static void main(String[] args) {
+		System.out.println('\'');
+		System.out.println("abc\t123\b456");	//	\b에 의해 3이 지워짐(이클립스에서는 안되고 터미널에선 됨)
+		System.out.println('\n');
+		System.out.println("\"Hello\"");
+		System.out.println("c:\\");
+	}
+
+}
