6a3a9c08a5a4bb1b5e70ab2f5cbc759f67561bd3
ysb4642 <ysb4642@naver.com>
2022-03-03 22:49:25 +0900

    22_03_03

---
+++ Exercise/src/Exercise6_24.java
@@ -0,0 +1,12 @@
+
+public class Exercise6_24 {
+	public static int abs(int value) {
+		return value >= 0 ? value : -value;
+	}
+	public static void main(String[] args) {
+		int value = 5;
+		System.out.println(value + "의 절대값 : " + abs(value));
+		value = -10;
+		System.out.println(value + "의 절대값 : " + abs(value));
+	}
+}
