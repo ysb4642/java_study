7e6a08c8c50e8699ba02cc1b029bf4b0c823c0bf
ysb4642 <ysb4642@naver.com>
2022-03-05 23:07:09 +0900

    22_03_05

---
+++ ch8/src/ExceptionEx15.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+public class ExceptionEx15 {
+
+	public static void main(String[] args) {
+		// command line에서 입력받은 값을 이름으로 갖는 파일을 생성한다.
+		File f = createFile(args[0]);
+		System.out.println(f.getName() + " 파일이 성공적으로 생성되었습니다.");
+
+	}
+	@SuppressWarnings("finally")
+	static File createFile(String fileName) {
+		try {
+			if (fileName == null || fileName.equals(""))
+				throw new Exception("파일 이름이 유효하지 않습니다.");
+		} catch (Exception e) {
+			// fileName이 부적절한 경우, 파일 이름을 제목없음.txt로 한다.
+			fileName = "제목없음.txt";
+		} finally {
+			File f = new File(fileName);		// File 클래스의 객체를 만든다.
+			createNewFile(f);						// 생성된 객체를 이용해서 파일을 생성한다.
+			return f;
+		}
+	}
+	
+	static void createNewFile(File f) {
+		try {
+			f.createNewFile();
+		} catch (Exception e) {}
+	}
+}
